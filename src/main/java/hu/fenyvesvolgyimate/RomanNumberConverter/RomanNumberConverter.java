4d16e4eb400138a4a08c7b5f38d644fe272b9835
fenyvesvolgyimate <mate.fenyvesvolgyi@lechnerkozpont.hu>
2022-11-27 23:18:05 +0100

    Check 4,5,6,7,8

--- src/main/java/hu/fenyvesvolgyimate/RomanNumberConverter/RomanNumberConverter.java
+++ src/main/java/hu/fenyvesvolgyimate/RomanNumberConverter/RomanNumberConverter.java
@@ -1,12 +1,30 @@
 package hu.fenyvesvolgyimate.RomanNumberConverter;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class RomanNumberConverter {
-    public int convertToRoman(int input) {
+    public String convertToRoman(int input) {
         checkInput(input);
-        return 0;
+        String result;
+        if (input <= 3)
+            result = printOneFor(input);
+        else if (input < 5)
+            result = printOnesBeforeFive(5 - input);
+        else if (input == 5)
+            result = "V";
+        else
+            result = printOnesAfterFive(input - 5);
+        return result;
+    }
+
+    private String printOnesAfterFive(int numbersOfOne) {
+        return "V" + printOneFor(numbersOfOne);
+    }
+
+    private String printOnesBeforeFive(int numbersOfOne) {
+        return printOneFor(numbersOfOne) + "V";
+    }
+
+    private String printOneFor(int input) {
+        return "I".repeat(Math.max(0, input));
     }
 
     private void checkInput(int input) {
--- src/test/java/RomanNumberConverterTests.java
+++ src/test/java/RomanNumberConverterTests.java
@@ -19,12 +19,52 @@ public class RomanNumberConverterTests {
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void ifZeroThenException(){
+    public void ifZeroThenException() {
         converter.convertToRoman(0);
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void ifMinusThenException(){
+    public void ifMinusThenException() {
         converter.convertToRoman(-1);
+    }
+
+    @Test
+    public void ifOneThenI() {
+        assertEquals("I", converter.convertToRoman(1));
+    }
+
+    @Test
+    public void ifTwoThenII() {
+        assertEquals("II", converter.convertToRoman(2));
+    }
+
+    @Test
+    public void ifThreeThenIII() {
+        assertEquals("III", converter.convertToRoman(3));
+    }
+
+    @Test
+    public void ifFiveThenV() {
+        assertEquals("V", converter.convertToRoman(5));
+    }
+
+    @Test
+    public void ifFourThenIV() {
+        assertEquals("IV", converter.convertToRoman(4));
+    }
+
+    @Test
+    public void ifSixThenVI() {
+        assertEquals("VI", converter.convertToRoman(6));
+    }
+
+    @Test
+    public void ifSevenThenVII() {
+        assertEquals("VII", converter.convertToRoman(7));
+    }
+
+    @Test
+    public void ifEightThenVIII() {
+        assertEquals("VIII", converter.convertToRoman(8));
     }
 }
