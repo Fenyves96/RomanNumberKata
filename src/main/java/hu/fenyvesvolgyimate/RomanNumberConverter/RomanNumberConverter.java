b93e02de21ed72066980d8e1ce5434d3bf416f77
fenyvesvolgyimate <mate.fenyvesvolgyi@lechnerkozpont.hu>
2022-11-27 22:22:38 +0100

    Convert RomanNumbers up to V except IV

--- src/main/java/hu/fenyvesvolgyimate/RomanNumberConverter/RomanNumberConverter.java
+++ src/main/java/hu/fenyvesvolgyimate/RomanNumberConverter/RomanNumberConverter.java
@@ -1,4 +1,28 @@
 package hu.fenyvesvolgyimate.RomanNumberConverter;
 
 public class RomanNumberConverter {
+    public int convert(String input) {
+        checkInput(input);
+        int result;
+        switch (input) {
+            case "I":
+                result = 1;
+                break;
+            case "II":
+                result = 2;
+                break;
+            case "III":
+                result = 3;
+                break;
+            default:
+                result = 5;
+                break;
+        }
+        return result;
+    }
+
+    private void checkInput(String input) {
+        if(input == null || input.equals(""))
+            throw new IllegalArgumentException();
+    }
 }
--- src/test/java/RomanNumberConverterTests.java
+++ src/test/java/RomanNumberConverterTests.java
@@ -1,11 +1,52 @@
 import hu.fenyvesvolgyimate.RomanNumberConverter.RomanNumberConverter;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class RomanNumberConverterTests {
+
+    private RomanNumberConverter converter;
+
+    @Before
+    public void setUp(){
+        converter = new RomanNumberConverter();
+    }
+
     @Test
-    public void testConstructorsWorks(){
+    public void testConstructorWorks(){
         RomanNumberConverter converter = new RomanNumberConverter();
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testConvertIfNullThenException(){
+        RomanNumberConverter converter = new RomanNumberConverter();
+        converter.convert(null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testConvertIfEmptyStringThenException(){
+        RomanNumberConverter converter = new RomanNumberConverter();
+        converter.convert("");
+    }
+
+    @Test()
+    public void testConvertIfRomanOneThenArabicOne(){
+        assertEquals(1, converter.convert("I"));
+    }
+
+    @Test()
+    public void testConvertIfRomanTwoThenArabicTwo(){
+        assertEquals(2, converter.convert("II"));
+    }
+
+    @Test()
+    public void testConvertIfRomanThreeThenArabicThree(){
+        assertEquals(3, converter.convert("III"));
+    }
+
+    @Test()
+    public void testConvertIfRomanFiveThenArabicFive(){
+        assertEquals(5, converter.convert("V"));
     }
 }
