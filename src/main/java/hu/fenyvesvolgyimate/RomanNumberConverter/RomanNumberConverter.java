2edcc92e54eb9fbde97e05ce25eea03ae3fa9523
fenyvesvolgyimate <mate.fenyvesvolgyi@lechnerkozpont.hu>
2022-11-27 21:49:13 +0100

    init project

---
+++ src/main/java/hu/fenyvesvolgyimate/RomanNumberConverter/RomanNumberConverter.java
@@ -0,0 +1,4 @@
+package hu.fenyvesvolgyimate.RomanNumberConverter;
+
+public class RomanNumberConverter {
+}
---
+++ src/test/java/RomanNumberConverterTests.java
@@ -0,0 +1,11 @@
+import hu.fenyvesvolgyimate.RomanNumberConverter.RomanNumberConverter;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class RomanNumberConverterTests {
+    @Test
+    public void testConstructorsWorks(){
+        RomanNumberConverter converter = new RomanNumberConverter();
+    }
+}
